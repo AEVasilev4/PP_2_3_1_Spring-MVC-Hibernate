68f39032b155cc9c0814257f906a91b371844f4f
Alexander Vasilev <sashaspb_2005@mail.ru>
2026-04-01 02:42:13 +0500

    commit1

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,29 @@
+package web.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
+
+@Controller
+public class CarController {
+
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping("/cars")
+    public String showCars(@RequestParam(required = false) Integer count, Model model) {
+        model.addAttribute("cars", carService.getCars(count));
+        return "cars";
+    }
+    @GetMapping("/")
+    public String showAllCars(Model model)  {
+        model.addAttribute("cars", carService.getCars(null));
+        return "cars";
+    }
+}
